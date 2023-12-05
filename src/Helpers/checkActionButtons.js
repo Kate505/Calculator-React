3a832b527269b657c66de4c89aaeacf47305a268
Kate505 <ksdelgado31@gmail.com>
2023-12-05 15:24:25 -0600

    Action Buttons fixed.

--- src/Helpers/checkActionButtons.js
+++ src/Helpers/checkActionButtons.js
@@ -5,16 +5,32 @@ export const checkActionButtons = (b, input, setInput, sign, setSign, count, set
 
   switch (b) {
     case 'CE':
-
+      if(input.length > 0){
+        const cadena = input.join('').split(sign.at(-1));
+        let result = '';
+        if(cadena[1].length > 0){
+          result = cadena[0] + sign.at(-1);
+          setInput(result.split(''));
+          setSign('');
+          setCount(0);
+        }else{
+          setInput('');
+          setSign('');
+          setCount(0);
+        }
+      }
       return true;
     case 'C':
       setInput('');
       setSign('');
       setCount(0);
-      console.log(input, ' H ',  sign)
       return true;
     case '⚡':
-
+      setSign('');
+      setCount(0);
+      input.pop();
+      setInput(input => [...input]);
+      console.log('sign: ', sign, ' input: ', input);
       return true;
     default:
         return false;
