473643e3da84e5ba709cf56e07c19f6e7742b28c
Kate505 <ksdelgado31@gmail.com>
2023-12-07 14:25:56 -0600

    Bug on buttons background color on mobile design fixed.

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -45,7 +45,7 @@ function Buttons({ b, setInput, input, sign, setSign, count, setCount, decimal, 
 
   return (
     <Box w='100%' h='20' alignItems='center' justifyContent='center' color='teal.500'  display='flex' borderWidth={1}
-         borderColor='teal.500' _hover={{color: 'white', textColor:'white', bg: 'teal.500'}} borderRadius={5}
+         borderColor='teal.500' borderRadius={5}
          _active={{bg: '#dddfe2', transform: 'scale(0.98)', borderColor: '#bec3c9'}} textStyle={{size: '100px'}}
          _focus={{boxShadow: '0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)'}}
          onClick={() => buttonClick(b,setInput, input, sign, setSign, count, setCount)}>
